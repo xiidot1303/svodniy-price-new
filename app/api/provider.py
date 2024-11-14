d538b6843349b59ab2c5e67adb3761a8adc37169
xiidot1303 <xii1303@inbox.ru>
2024-11-14 17:21:00 +0500

    Provider list api | drugs by provider api

--- app/api/provider.py
+++ app/api/provider.py
@@ -1,7 +1,10 @@
 from app.api import *
 from app.utils import *
-from app.services.drug_service import *
+from app.services.provider_service import *
 from adrf.views import APIView, AsyncRequest
+from app.serializers import *
+from swagger.responses import *
+
 
 class ProviderByName(APIView):
     @swagger_auto_schema(request_body=ProviderFilterSerializer, responses={status.HTTP_200_OK: ProviderSerializer})
@@ -11,5 +14,21 @@ class ProviderByName(APIView):
         if provider:
             serializer = ProviderSerializer(provider)
             return Response(await serializer.adata, status=status.HTTP_200_OK)
-            
+
         return Response({"error": "No providers found with the given title"}, status=status.HTTP_404_NOT_FOUND)
+
+
+class ProviderList(APIView):
+    @swagger_auto_schema(request_body=ProviderFilterSerializer, responses={status.HTTP_200_OK: ProviderSerializer(many=True)})
+    async def post(self, request: AsyncRequest):
+        name = request.data.get('name', None)
+        filter_serializer = ProviderFilterSerializer(request.data)
+        if filter_serializer.is_valid():
+            name = filter_serializer.validated_data.get('name', None)
+            providers = Provider.objects.filter(
+                **(await filter_provider_by_name(name))
+            )
+            serializer = ProviderSerializer(providers, many=True)
+            return Response(await serializer.adata, status=status.HTTP_200_OK)
+        return Response(filter_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
